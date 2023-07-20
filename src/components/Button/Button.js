35dce3543f7017007449baf5d0c2c726c60424b2
Jessica <jessica95.mp@gmail.com>
2023-07-19 22:13:39 -0500

    advance component buttom default and outline

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,13 @@
-const Button = () => {
-  return <div>Button</div>;
+// import { useState } from "react";
+import "./Button.css";
+
+const Button = ({ children = "Default", variant = "default" }) => {
+  // const [typeButton] = useState(variant);
+  // // setTypeButton(variant);
+  console.log({ variant });
+  // const setButton = () => {};
+  //
+  return <button className={variant}>{children}</button>;
 };
 
 export default Button;
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,31 @@ const Home = () => {
   return (
     <div className="container">
       <h1 className="title">Buttons</h1>
-      <Button />
+      <div className="container-buttons">
+        <div className="row-container">
+          <div>
+            <p>{" <Button />"}</p>
+            <Button />
+          </div>
+
+          <div>
+            <p>&:hover, &:focus</p>
+            <Button />
+          </div>
+        </div>
+
+        <div className="row-container">
+          <div>
+            <p>{" <Button variant=”outline” />"}</p>
+            <Button variant={"outline"} />
+          </div>
+
+          <div>
+            <p>&:hover, &:focus</p>
+            <Button variant={"outline"} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
