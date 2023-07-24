171ac4380106211eb6b7c8fbbad025326ceb3d6a
Jessica <jessica95.mp@gmail.com>
2023-07-23 21:48:36 -0500

    adpatcion de los estados del boton outline y text, colores

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,49 @@
 // import { useState } from "react";
 import "./Button.css";
 
-const Button = ({ children = "Default", variant = "default" }) => {
-  // const [typeButton] = useState(variant);
-  // // setTypeButton(variant);
-  console.log({ variant });
-  // const setButton = () => {};
-  //
-  return <button className={variant}>{children}</button>;
+const getCustomerButton = (color, variant, hover, disableShadow) => {
+  const className = [];
+
+  if (!variant) {
+    className.push(`button-${color}`);
+  }
+  if (variant === "outline") {
+    className.push(`button-${color}__outline button-outline `);
+  }
+  if (variant === "text") {
+    className.push(`button-${color}__outline button-text`);
+  }
+
+  if (hover) {
+    className.push(`button-active`);
+  }
+
+  if (!disableShadow) {
+    className.push(`shadow`);
+  }
+
+  return className.join(" ");
+};
+
+const Button = ({
+  label,
+  children = "Default",
+  variant,
+  color = "default",
+  hover = false,
+  disableShadow = false,
+}) => {
+  const classLabel = hover ? "label-hover" : "label";
+  const labelComponent = <label className={classLabel}>{label}</label>;
+
+  const classButton = getCustomerButton(color, variant, hover, disableShadow);
+
+  return (
+    <div>
+      {label ? labelComponent : ""}
+      <button className={classButton}>{children}</button>
+    </div>
+  );
 };
 
 export default Button;
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,27 +6,44 @@ const Home = () => {
       <h1 className="title">Buttons</h1>
       <div className="container-buttons">
         <div className="row-container">
-          <div>
-            <p>{" <Button />"}</p>
-            <Button />
-          </div>
-
-          <div>
-            <p>&:hover, &:focus</p>
-            <Button />
-          </div>
+          <Button label="<Button />" />
+          <Button label="&:hover, &:focus" hover={true} />
         </div>
 
         <div className="row-container">
-          <div>
-            <p>{" <Button variant=”outline” />"}</p>
-            <Button variant={"outline"} />
-          </div>
+          <Button
+            variant="outline"
+            color="primary"
+            label="<Button variant=”outline” />"
+          />
+          <Button
+            variant="outline"
+            label="&:hover, &:focus"
+            color="primary"
+            hover={true}
+          />
+        </div>
 
-          <div>
-            <p>&:hover, &:focus</p>
-            <Button variant={"outline"} />
-          </div>
+        <div className="row-container">
+          <Button
+            variant="text"
+            label="<Button variant=”text” />"
+            color="primary"
+          />
+          <Button
+            variant="text"
+            label="&:hover, &:focus"
+            hover={true}
+            color="primary"
+          />
+        </div>
+
+        <div className="row-container">
+          <Button
+            disableShadow={true}
+            label="<Button disableShadow />"
+            color="primary"
+          />
         </div>
       </div>
     </div>
